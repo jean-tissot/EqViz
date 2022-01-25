e8fffdfcbcecd315399be19f1448ba5a854f002d
Jean Tissot <asphjt@gmail.com>
2022-01-25 16:37:52 +0100

    update spack

--- eqviz-client/src/app/components/visualizers/spack-visualizer/spack-visualizer.component.ts
+++ eqviz-client/src/app/components/visualizers/spack-visualizer/spack-visualizer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Analyser } from 'src/app/objects/analyser';
 import { AudioService } from 'src/app/services/audio.service';
+import { Colors } from '../../utils/color';
 
 @Component({
   selector: 'app-spack-visualizer',
@@ -10,10 +11,16 @@ import { AudioService } from 'src/app/services/audio.service';
 export class SpackVisualizerComponent implements OnInit, OnDestroy {
 
   private analyser?: Analyser;
+  private nbFreqs = 5;
+  private data: number[][] = [];
+  private displayLength = 100;
 
   constructor(private audioService: AudioService) { }
 
   ngOnInit(): void {
+    for (let i = 0; i < this.nbFreqs; i++) {
+      this.data.push(new Array(this.displayLength).fill(0));
+    }
     var canvas = document.getElementById("spack-canvas") as HTMLCanvasElement;
     this.fitToContainer(canvas);
     var ctxCanvas = canvas?.getContext("2d");
@@ -49,40 +56,64 @@ export class SpackVisualizerComponent implements OnInit, OnDestroy {
       return
     }
 
-    requestAnimationFrame(() => this.draw(canvas, ctxCanvas, analyser));
+    var dataFreq = analyser.getFrequencyValues();
+    var nbFreqByStack = dataFreq.length / this.nbFreqs;
 
+    var precValue = 0
+    for (let i = 0; i < this.nbFreqs; i++) {
+      let value = dataFreq.slice(Math.round(nbFreqByStack * i), Math.round(nbFreqByStack * (i + 1))).reduce((a: number, b: number) => a + b) / nbFreqByStack;
+      value += precValue;
+      this.data[i].push(value);
+      precValue = value;
+    }
 
-    var dataTime = analyser.getFrequencyValues();
-    var dataSize = dataTime.length;
-
-    // ctxCanvas.fillStyle = 'rgb(200, 200, 200)';
+    ctxCanvas.fillStyle = 'rgb(10, 10, 10)';
     ctxCanvas.fillRect(0, 0, WIDTH, HEIGHT);
 
-    ctxCanvas.lineWidth = 2;
-    ctxCanvas.strokeStyle = 'rgb(200, 0, 200)';
+    ctxCanvas.lineWidth = 3;
+
+    var start = Math.max(0, this.data[0].length - this.displayLength)
+
+    var colors = Colors.generate(this.nbFreqs);
+    var ratio = 256.0 * this.nbFreqs;
+
+    for (let i = this.nbFreqs - 1; i >= 0; i--) {
+
+      ctxCanvas.strokeStyle = colors[i];
+      ctxCanvas.fillStyle = colors[i];
+
+
+      this.trace(this.data[i].slice(start, this.data[0].length), ratio, ctxCanvas);
+    }
+
+    requestAnimationFrame(() => this.draw(canvas, ctxCanvas, analyser));
+
+  }
+
+
+  private trace(data: number[], ratio: number, ctxCanvas: CanvasRenderingContext2D) {
+
+    var WIDTH = ctxCanvas.canvas.width;
+    var HEIGHT = ctxCanvas.canvas.height;
+    var sliceWidth = WIDTH * 1.0 / (this.displayLength - 1);
 
     ctxCanvas.beginPath();
-
-    var sliceWidth = WIDTH * 1.0 / dataSize;
+    ctxCanvas.moveTo(0, HEIGHT);
     var x = 0;
 
-    for (var i = 0; i < dataSize; i++) {
+    for (let value of data) {
 
-      var v = dataTime[i] / 128.0;
-      var y = (1 - v) * HEIGHT;
+      var y = (1 - value / ratio) * HEIGHT;
 
-      if (i === 0) {
-        ctxCanvas.moveTo(x, y);
-      } else {
-        ctxCanvas.lineTo(x, y);
-      }
+      ctxCanvas.lineTo(x, y);
 
       x += sliceWidth;
     }
 
-    ctxCanvas.lineTo(canvas.width, canvas.height / 2);
+    ctxCanvas.lineTo(WIDTH, HEIGHT);
+    ctxCanvas.closePath();
+    ctxCanvas.fill();
     ctxCanvas.stroke();
-
   }
 
 }
